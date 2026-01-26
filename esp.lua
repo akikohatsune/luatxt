ab46951841b69833c3485d25f454c778807297cd
AkikoC <k3212700@gmail.com>
2026-01-26 22:08:57 +0700

    Update and rename esp.lua.txt to esp.lua

--- esp.lua
+++ esp.lua
@@ -1348,3 +1348,14 @@ SkeletonTransparency:OnChanged(function(Value)
         end
     end
 end)
+--[[
+	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
+]]
+local Sprinting = game:GetService("ReplicatedStorage").Systems.Character.Game.Sprinting
+local stamina = require(Sprinting)
+stamina.MaxStamina = 100  -- Maximum stamina
+stamina.MinStamina = -20  -- Minimum stamina
+stamina.StaminaGain = 100 -- Stamina gain
+stamina.StaminaLoss = 5 -- Stamina loss
+stamina.SprintSpeed = 30 -- Sprint speed
+stamina.StaminaLossDisabled = true -- Disable stamina drain (true/false)
